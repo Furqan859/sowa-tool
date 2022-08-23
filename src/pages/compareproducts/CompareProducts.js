4921bc66e71bf82a5f32e8509c384cc9ed4e06ac
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-23 18:49:14 +0500

    sowa-tool website ca

--- src/pages/compareproducts/CompareProducts.js
+++ src/pages/compareproducts/CompareProducts.js
@@ -1,12 +1,31 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import './CompareProducts.css';
 
 const CompareProducts = () => {
+  const [data,setData] = useState([
+    {id: 1,username: 'furqan'},
+    {id: 2,username: 'usman'},
+    {id: 3,username: 'khizer'},
+    {id: 4,username: 'husnain'},
+    {id: 5,username: 'hafiz'},
+    {id: 6,username: 'sameet'},
+  ])
+
+const handeldelete =(id) => {
+const newData = data.filter(data => data.id !== id)
+setData(newData);
+}
+
   return (
-    <div>
-  <h1>Compare</h1>
-   <h4>No products to compare. Please select some products.</h4>
-    </div>
+   <div>
+   <h3>Furqan Rasool</h3>
+   <div>{data.map((da)=><div><div className="app">{da.id} . {da.username}</div>
+   <button onClick={()=>handeldelete(da.id)}>Delete</button></div>)}
+   
+   </div>
+   </div>
+  
   )
 }
 
